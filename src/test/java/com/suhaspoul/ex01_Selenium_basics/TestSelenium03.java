4f68522c124e0f6c71b6b0b8fc0e2b15a2f356d3
suhaspaul96 <suhasp0403@gmail.com>
2025-04-01 11:36:29 +0530

    Selenium01

---
+++ src/test/java/com/suhaspoul/ex01_Selenium_basics/TestSelenium03.java
@@ -0,0 +1,38 @@
+package com.suhaspoul.ex01_Selenium_basics;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestSelenium03 {
+
+    WebDriver driver;
+
+    @Test(priority = 1)
+    public void verify_login(){
+
+        driver = new ChromeDriver();
+        driver.get("https://practicetestautomation.com/practice-test-login/");
+        System.out.println(driver.getTitle());
+        driver.manage().window().maximize();
+        //driver.quit();
+
+
+    }
+    @Test(priority = 2)
+    public void verify_submit(){
+
+        driver.findElement(By.id("username")).sendKeys("student");
+        driver.findElement(By.id("password")).sendKeys("Password123");
+        driver.findElement(By.id("submit")).click();
+        System.out.println(driver.getTitle());
+        //driver.quit();
+
+    }
+    @Test(priority = 3)
+    public void verify_logout(){
+        driver.findElement(By.xpath("//a[@class='wp-block-button__link has-text-color has-background has-very-dark-gray-background-color']")).click();
+    }
+
+}
